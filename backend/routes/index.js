7eab904b754d6fc4e3af9e5669992ac4e23d885a
eating <eva930514@gmail.com>
2023-01-24 20:13:16 +0800

    backend init

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./sqlite3.db');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
